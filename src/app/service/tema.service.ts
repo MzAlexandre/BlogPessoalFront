e979e3ca76080696c91712e45cb9b83376186462
Marco Alexandre <marcoalexandre554@gmail.com>
2022-04-05 08:48:55 -0300

    @MzAlexandre ADD Put e Delete do tema part II

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,20 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogpessoalmarco.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoalmarco.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoalmarco.herokuapp.com/tema', tema, this.token)
   } 
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessoalmarco.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://blogpessoalmarco.herokuapp.com/tema/${id}`, this.token)
+  }
 
 }
